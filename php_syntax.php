39ccd1631434b6b422f88aa7beb94ba895c56f58
saphire001 <spj29@njit.edu>
2021-02-10 17:33:54 -0500

    php basics

--- php_syntax.php
+++ php_syntax.php
@@ -1,16 +1,16 @@
 <?php
 echo "PHP [Syntax] Exercise [1] <br>\n";
-echo "echo"; 
+echo "echo <br>\n"; 
 
 echo "PHP [Syntax] Exercise [2] <br>\n";
-echo "<?php, ?>";
+echo "< ? php, ? > <br>\n>";
 
 echo "PHP [Syntax] Exercise [3] <br>\n"; 
-echo "//";
+echo "// <br>\n";
 
 echo "PHP [Syntax] Exercise [4] <br>\n";
-echo "'/*, */'";
+echo "'/*, */' <br>\n";
 
 echo "PHP [Syntax] Exercise [5] <br>\n";
-echo ";";
+echo "; <br>\n";
 ?>
