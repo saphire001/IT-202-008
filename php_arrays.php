5ffd0335ef3aa96cdfa3ff18deb7d1d9d1f2e32c
saphire001 <spj29@njit.edu>
2021-02-10 16:51:31 -0500

    php basic

---
+++ php_arrays.php
@@ -0,0 +1,25 @@
+<?php
+echo "PHP [array] Exercise [1] <br>\n";
+echo "count($fruits)"; 
+
+echo "PHP [array] Exercise [2] <br>\n";
+echo "$fruits[1]"; 
+
+echo "PHP [array] Exercise [3] <br>\n";
+echo "=>, =>, =>"; 
+
+echo "PHP [array] Exercise [4] <br>\n";
+echo "$age['Ben']"; 
+
+echo "PHP [array] Exercise [5] <br>\n";
+echo "fareach, as, =>, $x, $y"; 
+
+echo "PHP [array] Exercise [6] <br>\n";
+echo "sort($colors)"; 
+
+echo "PHP [array] Exercise [7] <br>\n";
+echo "rsort($colors)"; 
+
+echo "PHP [array] Exercise [8] <br>\n";
+echo "asort($age)"; 
+?> 
---
+++ php_syntax.php
@@ -0,0 +1,16 @@
+<?php
+echo "PHP [Syntax] Exercise [1] <br>\n";
+echo "echo"; 
+
+echo "PHP [Syntax] Exercise [2] <br>\n";
+echo "<?php, ?>";
+
+echo "PHP [Syntax] Exercise [3] <br>\n"; 
+echo "//";
+
+echo "PHP [Syntax] Exercise [4] <br>\n";
+echo "'/*, */'";
+
+echo "PHP [Syntax] Exercise [5] <br>\n";
+echo ";";
+?>
