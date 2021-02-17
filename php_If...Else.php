39a7170034bdc178209b14e3c47f2122cca6bf23
saphire001 <spj29@njit.edu>
2021-02-17 12:25:26 -0500

    php flow control

---
+++ php_If...Else.php
@@ -0,0 +1,13 @@
+<?php
+echo "PHP Exercise [If...Else] Exercise [1]<br>\n";
+echo "if, ($ a, $ b) <br>\n";
+
+echo "PHP Exercise [If...Else] Exercise [2]<br>\n";
+echo "if, ($ a, !=, $ b) <br>\n"; 
+
+echo "PHP Exercise [If...Else] Exercise [3]<br>\n";
+echo "if,else <br>\n"; 
+
+echo "PHP Exercise [If...Else] Exercise [4]<br>\n";
+echo "if, elseif, else <br>\n"; 
+?>
