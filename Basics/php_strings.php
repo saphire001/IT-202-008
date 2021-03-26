2110a959bfe126af903236e1d443cab3b97bef08
saphire001 <spj29@njit.edu>
2021-03-26 16:59:13 -0400

    Basics

---
+++ Basics/php_strings.php
@@ -0,0 +1,10 @@
+<?php
+echo "PHP [String] Exercise [1] <br>\n";
+echo "strlen <br>\n"; 
+
+echo "PHP [String] Exercise [2] <br>\n";
+echo "strrev <br>\n"; 
+
+echo "PHP [String] Exercise [3] <br>\n";
+echo "str_replace, 'World', 'Dolly', '$ oldtext' <br>\n";
+?>
