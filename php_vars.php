9813c78312ac045e7fd275480fa7de057d3dded1
saphire001 <spj29@njit.edu>
2021-03-26 17:03:37 -0400

    d

--- php_vars.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "PHP [Variables] Exercise [1] <br>\n";
-echo "$ txt = 'Hello' <br>\n"; 
-
-echo "PHP [Variables] Exercise [2] <br>\n";
-echo "$ x = 5, $ y = 7, echo $ x + $ y <br>\n";
-?>
-
