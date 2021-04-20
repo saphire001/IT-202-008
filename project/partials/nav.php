1f672317e61f6275c73092b6bd0cacb516bfdb98
saphire001 <spj29@njit.edu>
2021-04-20 18:37:24 -0400

    tables

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,8 +1,8 @@
-<link rel="stylesheet" href="static/css/styles.css">
 <?php
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
+<link rel="stylesheet" href="static/css/styles.css">
 <nav>
 <ul class="nav">
     <li><a href="home.php">Home</a></li>
