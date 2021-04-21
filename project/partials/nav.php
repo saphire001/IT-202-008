ff0433c72690c67de72f07492ce8ce7f94df0135
saphire001 <spj29@njit.edu>
2021-04-21 14:03:46 -0400

    test route

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -12,7 +12,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
-        <li><a href="#">Create Account</a></li>
+        <li><a href="test_create_accounts.php">Create Account</a></li>
         <li><a href="#">Accounts</a></li> 
         <li><a href="#">Transfer</a></li>
         <li><a href="logout.php">Logout</a></li>
