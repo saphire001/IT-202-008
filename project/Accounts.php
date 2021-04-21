9e570bee22f43d0196fbd9caf031c50024653607
saphire001 <spj29@njit.edu>
2021-04-21 19:22:36 -0400

    Account

---
+++ project/Accounts.php
@@ -0,0 +1,9 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+$db = getDB();
+$U_id =  get_user_id();
+
+echo $U_id; 
+
+?>
+<?php require(__DIR__ . "/partials/flash.php");?>
--- project/test_edit_accounts.php
+++ project/test_edit_accounts.php
@@ -61,4 +61,4 @@ if(isset($id)){
 }
 ?>
 
-<?php require(__DIR__ . "/partials/flash.php");
+<?php require(__DIR__ . "/partials/flash.php");?>
