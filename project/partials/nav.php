2e49d6fdbfd06d4aa94480e07c9016f45b564e28
saphire001 <spj29@njit.edu>
2021-04-21 19:25:08 -0400

    nav

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -13,7 +13,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="test_create_accounts.php">Create Account</a></li>
-        <li><a href="#">Accounts</a></li> 
+        <li><a href="Accounts.php">Accounts</a></li> 
         <li><a href="#">Transfer</a></li>
         <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
