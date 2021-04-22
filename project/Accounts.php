e2bf7f67d70a328642929a5f598d12a945b24b8f
saphire001 <spj29@njit.edu>
2021-04-21 20:07:34 -0400

    test

--- project/Accounts.php
+++ project/Accounts.php
@@ -2,28 +2,8 @@
 <?php
 $db = getDB();
 $U_id =  get_user_id();
-$AN = $_POST["account_number"]; 
-$ATy = $_POST["account_type"]; 
-$BA = $_POST["balance"]; 
 
-$stmt = $db -> prepare ("SELECT account_number, account_type, balance WHERE user_id = :user"); 
-$stmt->execute([":user" => get_user_id()]);
-$r = $stmt->fetch(PDO::FETCH_ASSOC);
-
-if($r)
-{
-    $AN = $r['account_number'];
-     $ATy = $r['accunt_type'];
-     $BA = $r['balance']; 
-}
-else
-{
-
-}
-
-echo $AN;
-echo $ATy;
-echo $BA;
+echo $U_id; 
 
 ?>
 <?php require(__DIR__ . "/partials/flash.php");?>
