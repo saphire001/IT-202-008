4ebd3812d18cb4d13297d89062a55f86891e6e3a
saphire001 <spj29@njit.edu>
2021-05-06 23:05:02 -0400

    rT

--- project/Transactions.php
+++ project/Transactions.php
@@ -72,32 +72,6 @@ if (!is_logged_in()) {
 </ul> 
 
 <form method="POST">
-  <?php if (count($results) > 0): ?>
-  <div class="form-group">
-    <label for="account"><?php echo $type == 'transfer' ? 'Account Source' : 'Account'; ?></label>
-    <select class="form-control" id="account" name="<?php echo $type == 'transfer' ? 'account_src' : 'account'; ?>">
-      <?php foreach ($results as $r): ?>
-      <?php if ($r["account_type"] != "loan"): ?>
-      <option value="<?php safer_echo($r["id"]); ?>">
-        <?php safer_echo($r["account_number"]); ?> | <?php safer_echo($r["account_type"]); ?> | <?php safer_echo($r["balance"]); ?>
-      </option>
-      <?php endif; ?>
-      <?php endforeach; ?>
-    </select>
-  </div>
-  <?php endif; ?>
-  <?php if (count($results) > 0 && $type == 'transfer'): ?>
-  <div class="form-group">
-    <label for="account">Account Destination</label>
-    <select class="form-control" id="account" name="account_dest">
-      <?php foreach ($results as $r): ?>
-      <option value="<?php safer_echo($r["id"]); ?>">
-        <?php safer_echo($r["account_number"]); ?> | <?php safer_echo($r["account_type"]); ?> | <?php safer_echo($r["balance"]); ?>
-      </option>
-      <?php endforeach; ?>
-    </select>
-  </div>
-  <?php endif; ?>
   <div class="form-group">
     <label for="deposit">Amount</label>
     <div class="input-group">
