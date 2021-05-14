267ae10af46754c2ce8f50f25a974cb69a3bfbb5
saphire001 <spj29@njit.edu>
2021-05-13 20:57:34 -0400

    admin --> login

--- project/test_create_accounts.php
+++ project/test_create_accounts.php
@@ -1,10 +1,10 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
-if (!has_role("Admin")) {
+if (!is_logged_in()) {
     //this will redirect to login and kill the rest of this script (prevent it from executing)
     flash("You don't have permission to access this page");
     die(header("Location: login.php"));
-}
+  }
 ?>
 <?php
 if(isset($_POST["create"])){
